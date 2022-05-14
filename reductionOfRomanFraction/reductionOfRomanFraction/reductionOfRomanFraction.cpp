95bcd31f4db7aad83fb653d1b8c671553930aeee
Saelaress <93143602+Saelaress@users.noreply.github.com>
2022-05-14 15:12:23 +0400

    Programmed the reductionOfRomanFraction function
    Programmed the reductionOfRomanFraction function. Added helper functions.

--- reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.cpp
+++ reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.cpp
@@ -36,8 +36,20 @@ int main(int argc, char** argv)
 
     fin.close();
 
+    string newFraction;
+    try
+    {
     //Сократить дробь
-    string newFraction = reductionOfRomanFraction(fraction);
+    newFraction = reductionOfRomanFraction(fraction);
+    }
+
+    //Обработать исключения
+    catch (int ex)
+    {
+        if (ex == 7)
+            cout << "Несократимая дробь!" << endl;
+    }
+
     //Записать сокращенную дробь в выходной файл
     string outPath = "C:\\Users\\User\\Desktop\\Universitet\\2_kurs_2_sem\\knpo\\reducedFraction.txt";
     
@@ -45,13 +57,77 @@ int main(int argc, char** argv)
     fout.open(outPath);
     fout << newFraction;
     fout.close();
-
-    //Обработать исключения
-
 }
 
 string reductionOfRomanFraction(string romanFraction)
 {
-    string reducedFraction = "III";
-    return reducedFraction;
+    //Проверить строку на корректность
+    isContentOfStringCorrect(romanFraction);
+
+    //Разделить строку на числитель и знаменатель
+    string numerator;
+    string denominator;
+
+    auto pos = romanFraction.find("/");
+    if (pos != string::npos)
+    {
+        numerator = romanFraction.substr(0, pos);
+        denominator = romanFraction.substr(pos + 1);
+    }
+
+    //Проверить отвечают ли числитель и знаменатель принципам РСС
+    isRomNumCorrect(numerator);
+    isRomNumCorrect(denominator);
+
+    //Перевести числитель и знаменатель из РСС в ДСС
+    int decNumerator = convertNumFromRomToDec(numerator);
+    int decDenominator = convertNumFromRomToDec(denominator);
+
+    //Найти наибольший общий делитель(НОД)
+    int GCD = findGCD(decNumerator, decDenominator);
+
+    //Если НОД равен 1, и не равен знаменателю, выбросить исключение
+    if (GCD = 1)
+    {
+        throw 7;
+    }
+
+    //Разделить числитель и знаменатель на НОД
+    int newDecNumerator = decNumerator / GCD;
+    int newDecDenominator = decDenominator / GCD;
+
+    //Перевести из ДСС в РСС оба числа
+    string newNumerator = convertNumFromDecToRom(newDecNumerator);
+    string newDenominator = convertNumFromDecToRom(newDecDenominator);
+
+    string reducedFraction = newNumerator + "/" + newDenominator;
+    //Если знаменатель равен I
+    if (newDenominator == "I")
+    {
+        //Вернуть числитель в Римской СС
+        return newNumerator;
+    }
+
+    //Иначе вернуть строку из числителя, знака "/" и знаменателя без пробелов
+    else return reducedFraction;
+}
+
+void isContentOfStringCorrect(string romanFraction) {
+
+}
+
+void isRomNumCorrect(string romNum) {
+
+}
+
+int convertNumFromRomToDec(string romNum) {
+    return 0;
+}
+
+int findGCD(int numerator, int denominator) {
+    return 0;
+}
+
+string convertNumFromDecToRom(int decNum) {
+    return 0;
 }
--- reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.h
+++ reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.h
@@ -7,3 +7,37 @@ using namespace std;
 *	\throw - (Выбрасывает исключения в случае ошибок)
 */
 string reductionOfRomanFraction(string romanFraction);
+
+/*! Проверяет содержимое строки на корректность
+*	\param[in] romanFraction - дробь в римской СС
+*	\throw - (Выбрасывает исключения в случае ошибок)
+*/
+void isContentOfStringCorrect(string romanFraction);
+
+/*! Проверяет отвечает ли число приципам РСС
+*	\param[in] romNum - римское число
+*	\throw - (Выбрасывает исключения в случае ошибок)
+*/
+void isRomNumCorrect(string romNum);
+
+/*! Переводит число из РСС в ДСС
+*	\param[in] romNum - римское число
+*	\return - десятичное число
+*	\throw - (Выбрасывает исключения в случае ошибок)
+*/
+int convertNumFromRomToDec(string romNum);
+
+/*! Находит наибольший общий делитель (НОД)
+*	\param[in] numerator - числитель
+*	\param[in] denominator - знаменатель
+*	\return - НОД
+*	\throw - (Выбрасывает исключения в случае ошибок)
+*/
+int findGCD(int numerator, int denominator);
+
+/*! Переводит число из ДСС в РСС
+*	\param[in] romNum - десятичное число
+*	\return - римское число
+*	\throw - (Выбрасывает исключения в случае ошибок)
+*/
+string convertNumFromDecToRom(int decNum);
