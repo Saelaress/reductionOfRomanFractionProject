a1297b91b702d35db915c7e57c2af4740876043e
Saelaress <93143602+Saelaress@users.noreply.github.com>
2022-06-02 23:47:22 +0400

    Improved error handling

--- reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.cpp
+++ reductionOfRomanFraction/reductionOfRomanFraction/reductionOfRomanFraction.cpp
@@ -11,21 +11,28 @@ int main(int argc, char** argv)
     setlocale(LC_ALL, "rus");
 
     //Выдать ошибку, если входной файл не указан в аргументах командной строки
-    if (argc == 1)
+    if (argc < 2)
     {
-        cout << "Входной файл не указан." << endl;
+        cout << "Program's argument is empty. Enter name of the input file that contains roman fraction" << endl;
         return 0;
     }
-    
+
+    //Выдать ошибку, если выходной файл не указан в аргументах командной строки
+    if (argc < 3)
+    {
+        cout << "Program's argument is empty. Enter name of the output file" << endl;
+        return 0;
+    }
+
     //Выдать ошибку, если входной файл невозможно открыть
-    string path = argv[1];
+    string inputPath = argv[1];
 
     ifstream fin;
-    fin.open(path);
+    fin.open(inputPath);
 
     if (!fin.is_open())
     {
-        cout << "Входной файл " << path << " невозможно открыть." << endl;
+        cout << "Input file was not opened!" << endl;
         return 0;
     }
 
@@ -33,7 +40,20 @@ int main(int argc, char** argv)
     string fraction;
 
     getline(fin, fraction);
-    cout << fraction << endl;
+    
+    //Выдать ошибку, если в файл пустой
+    if (fraction == "")
+    {
+        cout << "There is no data in the input file. Enter a fraction in the Roman numeral system!" << endl;
+        return 0;
+    }
+
+    //Выдать ошибку, если в файле больше одной строки
+    if (!fin.eof())
+    {
+        cout << "The program accepts files from one line as input. Place each line in a separate file." << endl;
+        return 0;
+    }
 
     fin.close();
     
@@ -47,30 +67,48 @@ int main(int argc, char** argv)
     //Обработать исключения
     catch (int ex)
     {
-        if (ex == 1)
-            cout << "Ошибка в записи римской дроби!" << endl;
-        if (ex == 2)
-            cout << "Символы V,L,D не могут повторяться!" << endl;
-        if (ex == 3)
-            cout << "Символы I,X,C не могут повторяться больше 3х раз!" << endl;
-        if (ex == 4)
-            cout << "Недопустимы сочетания: IL, IC, ID, IM, VX, VL, VC, VD, VM, XD, XM, LC, LD, LM, DM" << endl;
-        if (ex == 5)
-            cout << "Больший символ не может находиться между двумя одинаковыми меньшими!" << endl;
-        if (ex == 6)
-            cout << "Больший символ не может быть записан после двух меньших!" << endl;
-        if (ex == 7)
-            cout << "Несократимая дробь!" << endl;
-        if (ex == 8)
-            cout << "Число больше максимально допустимого!" << endl;
+        switch (ex)
+        {
+        case 1:
+            cout << "Error in writing Roman fractions!" << endl;
+            break;
+        case 2:
+            cout << "Characters V,L,D cannot be repeated!" << endl;
+            break;
+        case 3:
+            cout << "Symbols I,X,C cannot be repeated more than 3 times!" << endl;
+            break;
+        case 4:
+                cout << "Combinations not allowed: IL, IC, ID, IM, VX, VL, VC, VD, VM, XD, XM, LC, LD, LM, DM." << endl;
+                break;
+        case 5:
+                cout << "A larger symbol cannot be between two identical smaller ones!" << endl;
+                break;
+        case 6:
+            cout << "A larger character cannot be written after two smaller ones!" << endl;
+            break;
+        case 7:
+            cout << "Irreducible fraction!" << endl;
+            break;
+        case 8:
+            cout << "The number is greater than the maximum!" << endl;
+            break;
+        }
         return 0;
     }
 
     //Записать сокращенную дробь в выходной файл
-    string outPath = "C:\\Users\\User\\Desktop\\Universitet\\2_kurs_2_sem\\knpo\\reducedFraction.txt";
+    string outPath = argv[2];
     
     ofstream fout;
     fout.open(outPath);
+
+    if (!fout.is_open())
+    {
+        cout << "Output file was not opened!" << endl;
+        return 0;
+    }
+
     fout << newFraction;
     fout.close();
 }
